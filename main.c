b0d34edf971e034795942ed4f870ff3113c16a5e
Jon Arintok <jon.arintok@gmail.com>
2018-12-14 19:38:25 +0200

    not right

--- main.c
+++ main.c
@@ -5,7 +5,6 @@
 #include <math.h>
 #include <stdbool.h>
 #include <SDL2/SDL.h>
-#include "options.h"
 
 static void sdlec(int line, char const *file) {
 	char const *error = SDL_GetError();
@@ -49,22 +48,43 @@ void logSpec(SDL_AudioSpec const as) {
 	);
 }
 
-double tonePhase = 0;
 void audioCallback(void *_unused, uint8_t *byteStream, int byteStreamLength) {
 	float *floatStream = (float*)byteStream;
-	double const toneInc = (2.0*freqFromPitch(tonePitch))/sampleRate;
+	double static tonePhase = 0;
+	double static const tonePitch = 57;
+	double const toneInc = freqFromPitch(tonePitch)/sampleRate;
 	for (int s = 0; s < floatStreamSize; s += 2) {
 		// generate saw wave
 		tonePhase -= toneInc;
-		if (tonePhase < -1) {
-			tonePhase += 2;
+		if (tonePhase < 0) {
+			tonePhase += 1;
 		}
 		double sample = tonePhase;
-		// LP filter
-		
+		// filter
+		{
+			double static const LPFCutoffFreq = 880;
+			double static const LPFResonance = 0.5;
+			double static const p = LPFCutoffFreq + 0.8*LPFCutoffFreq*(1.0 - LPFCutoffFreq);
+			double static const f = p + p - 1.0;
+			double const q = 0.5*(1.0 + 0.5*q*(1.0 - q + 5.6*q*q));
+			double static b0=0, b1=0, b2=0, b3=0, b4=0;
+			double const in = sample - q*b4;
+			double t1 = b1;
+			double t2 = b2;
+			b1 = (in + b0)*p - b1*f;
+			b2 = (b1 + t1)*p - b2*f;
+			t1 = b3;
+			b3 = (b2 + t2)*p - b3*f;
+			b4 = (b3 + t1)*p - b4*f;
+			b4 = b4 - b4*b4*b4*(5.0/3.0); //clipping
+			b0 = in;
+			sample = b4; // Lowpass output
+			//sample = in - b4; // Highpass output
+			//sample = 3.0f*(b3 - b4); // Bandpass output
+		}
 		// write to buffer
-		floatStream[s  ] = sample;
-		floatStream[s+1] = sample;
+		floatStream[s  ] = sample*2 - 1;
+		floatStream[s+1] = sample*2 - 1;
 	}
 }
 
@@ -92,7 +112,7 @@ int init(void) {
 	audioDeviceId = SDL_OpenAudioDevice(NULL, 0, &want, &audioSpec, 0);_sdlec;
 	puts("wanted audioSpec:");
 	logSpec(want);
-	puts("aquired audioSpec:");
+	puts("acquired audioSpec:");
 	logSpec(audioSpec);
 	if (!audioDeviceId) return 1;
 	sampleRate = audioSpec.freq;
--- options.h
+++
@@ -1,5 +0,0 @@
-
-#define tonePitch      57
-#define LPFCutoffFreq 880
-#define LPFResonance    0.5
-
